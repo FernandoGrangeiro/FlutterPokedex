733f4100ae05722a4b0e748d5d474557c578eaf7
Rafael Teruya <teruya.dev@gmail.com>
2020-01-26 02:53:09 -0300

    Squashed commit of the following:
    commit 1ffe5635bab9d838cb62c82d76faaec4e1e43cba
    Author: Rafael Teruya <teruya.dev@gmail.com>
    Date:   Sun Jan 26 02:52:01 2020 -0300
    
        shiny

--- lib/app/modules/pokemonDetails/models/pokemon.dart
+++ lib/app/modules/pokemonDetails/models/pokemon.dart
@@ -10,14 +10,16 @@ class Pokemon {
     this.image = image;
     this.order = order;
     this.stats = stats;
+    this.shinyImage = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/$id.png";
   }
   int id;
   TypeInfo typeInfo;
   String name;
   String image;
+  String shinyImage;
   int order;
   Stats stats;
 
   Pokemon.fromJson(Map<String, dynamic> json, TypeInfo type, Stats stats,)
-      : id = json['id'], typeInfo = type, stats = stats, image = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'+ json['id'].toString() +'.png', order = json['order'], name = json['name'];
+      : id = json['id'], typeInfo = type, stats = stats, image = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'+ json['id'].toString() +'.png', shinyImage = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/' + json['id'].toString() + '.png', order = json['order'], name = json['name'];
 }
--- lib/app/modules/pokemonDetails/utils/calculateContrastColor.dart
+++ lib/app/modules/pokemonDetails/utils/calculateContrastColor.dart
@@ -13,6 +13,21 @@ double calculateLuminescenceFactor(int colorComponent) {
   if (value <= 0.03928) {
     return value / 12.92;
   } else {
-    pow((value + 0.055)/ 1.055, 2.4);
+    return pow((value + 0.055)/ 1.055, 2.4);
   }
+}
+
+double contrast(List<int> rgb1, List<int> rgb2) {
+  double l1 = luminescence(rgb1[0], rgb1[1], rgb1[2]);
+  double l2 = luminescence(rgb2[0], rgb2[1], rgb2[2]);
+  return max(l1, l1)/ min(l1, l2);
+}
+
+Color calculateContrastColor(Color color) {
+  double blackContrast = contrast([color.red, color.green, color.blue], [0, 0, 0]);
+  double whiteContrast = contrast([color.red, color.green, color.blue], [0, 0, 0]);
+  if (whiteContrast > blackContrast) {
+    return Colors.white;
+  }
+  return Colors.black;
 }
